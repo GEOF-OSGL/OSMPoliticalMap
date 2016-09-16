c3a8edb10cff5e60d438b4275a30150b33e612b0
Tomislav Jogun <tjogun@geof.hr>
2016-09-16 12:09:49 +0200

    Update _0_PoliticalMap.sh

--- _0_PoliticalMap.sh
+++ _0_PoliticalMap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir ./PK
+cd ./PK
 echo "Starting download of the OSM planet file ..."
 wget http://ftp5.gwdg.de/pub/misc/openstreetmap/planet.openstreetmap.org/pbf/planet-latest.osm.pbf --output-document=planet.osm.pbf
 echo "Starting conversion of pbf to o5m ..."
@@ -8,6 +9,7 @@ osmconvert --drop-author planet.osm.pbf -o=planet.osm.o5m
 rm -f planet.osm.pbf
 echo "Conversion of pbf to o5m done."
 echo "-------------------------------------------------"
+cd ..
 
 ./_1_Graticule.sh
 ./_2_Coastlines.sh
