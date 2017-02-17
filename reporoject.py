b61a1467e44defff3e406472889fd0e71fc771db
Dražen Tutić <dtutic@geof.hr>
2017-02-17 11:31:57 +0100

    Update reporoject.py

--- reporoject.py
+++ reporoject.py
@@ -1,47 +1,55 @@
+#Copyright (C) 2016, 2017 Drazen Tutic, Tomislav Jogun, Ana Kuvezdic Divjak
+#This file is part of OSMPoliticalMap software.
+#
+#OSMPoliticalMap is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#OSMPoliticalMap is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with OSMPoliticalMap.  If not, see <http://www.gnu.org/licenses/>.
+
 from osgeo import ogr, osr
 import os,sys
 from pyproj import Proj
 import numpy as np
 
-
-
-wintri=Proj(proj='wintri',ellps='WGS84',lat_1='50.4597762522')
+map_proj=Proj(os.getenv('OSM_PROJ'))
 
 driver = ogr.GetDriverByName('ESRI Shapefile')
-
 
 def Reproject_Ring(g):
     p_len = g.GetPointCount()
     points = np.zeros((p_len,2))
     for i in range(0, p_len):
         p = g.GetPoint(i)
-        points[i,0],points[i,1] = wintri(p[0],p[1])
-       
+        points[i,0],points[i,1] = map_proj(p[0],p[1])   
     ring = ogr.Geometry(ogr.wkbLinearRing)    
     for i in range (0,p_len):
         ring.AddPoint(points[i,0], points[i,1])
     return ring
-
 
 def Reproject_Line(g):
     p_len = g.GetPointCount()
     points = np.zeros((p_len,2))
     for i in range(0, p_len):
         p = g.GetPoint(i)
-        points[i,0],points[i,1] = wintri(p[0],p[1])
-       
+        points[i,0],points[i,1] = map_proj(p[0],p[1])       
     line = ogr.Geometry(ogr.wkbLineString)    
     for i in range (0,p_len):
         line.AddPoint(points[i,0], points[i,1])
     return line
 
-
 def Reproject_Point(g):
     points = np.zeros((1,2)) 
     for i in range(0,1):
         p = g.GetPoint(i)
-        points[i,0],points[i,1] = wintri(p[0],p[1])
-       
+        points[i,0],points[i,1] = map_proj(p[0],p[1])       
     point = ogr.Geometry(ogr.wkbPoint)
     for i in range(0,1):
         point.AddPoint(points[i,0], points[i,1])
@@ -68,7 +76,6 @@ for i in range(0, inLayerDefn.GetFieldCount()):
 outLayerDefn = outLayer.GetLayerDefn()
 
 # loop through the input features
-#num = 0
 feature = inLayer.GetNextFeature()
 while feature:
     geom = feature.GetGeometryRef()
@@ -78,44 +85,25 @@ while feature:
             poly = ogr.Geometry(ogr.wkbPolygon) 
             g = geom.GetGeometryRef(i) #polygon can have multiple rings
             for j in range(0, g.GetGeometryCount()): #iterate over rings
-                #if num % 10000 == 0:
-                #     print num,'\r'
-                #num = num +1
                 ring = g.GetGeometryRef(j) #access to a ring (closed polyline)
                 ring_proj = Reproject_Ring(ring) 
                 poly.AddGeometry(ring_proj)
             geom_proj.AddGeometry(poly)                      
-        #feature.SetGeometryDirectly(multipolygon)        
     elif geom.GetGeometryName() == 'POLYGON':
         geom_proj = ogr.Geometry(ogr.wkbPolygon) 
         for i in range(0, geom.GetGeometryCount()): #iterate over rings
             g = geom.GetGeometryRef(i) #access to a ring (closed polyline)
-            #if num % 10000 == 0:
-            #    print num,'\r'
-            #num = num +1
             ring_proj = Reproject_Ring(g)
             geom_proj.AddGeometry(ring_proj)
-        #feature.SetGeometryDirectly(poly) 
     elif geom.GetGeometryName() == 'MULTILINESTRING':
         geom_proj = ogr.Geometry(ogr.wkbMultiLineString)
         for i in range(0, geom.GetGeometryCount()): #iterate over lines
-            #if num % 10000 == 0:
-            #    print num,'\r'
-            #num = num +1
             g = geom.GetGeometryRef(i) 
             line_proj = Reproject_Line(g) 
             geom_proj.AddGeometry(line_proj)
-        #feature.SetGeometryDirectly(multiline)        
     elif geom.GetGeometryName() == 'LINESTRING':
-        #if num % 10000 == 0:
-        #    print num,'\r'
-        #num = num +1
         geom_proj = Reproject_Line(geom)
-        #feature.SetGeometryDirectly(line_proj)
     elif geom.GetGeometryName() == 'POINT':
-        #if num % 10000 == 0:
-        #    print num,'\r'
-        #num = num +1
         geom_proj = Reproject_Point(geom)
     # create a new feature
     outFeature = ogr.Feature(outLayerDefn)
@@ -131,7 +119,7 @@ while feature:
     feature = inLayer.GetNextFeature()
 
 
-#create .prj file with fake projection (Plate Caree) instead of Winkel Tripel to avoid geographic CRS in QGIS
+#create .prj file with fake projection (Plate Caree) instead of given one to avoid geographic CRS in QGIS
 srs = osr.SpatialReference()
 srs.ImportFromProj4('+proj=eqc +ellps=WGS84')
 if srs is not None:
