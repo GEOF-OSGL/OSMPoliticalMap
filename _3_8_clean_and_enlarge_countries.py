85b55c9e4d4c3eaa5ef6fdf8c1cef325f8add216
Dražen Tutić <dtutic@geof.hr>
2017-02-17 11:22:41 +0100

    Update _3_8_clean_and_enlarge_countries.py

--- _3_8_clean_and_enlarge_countries.py
+++ _3_8_clean_and_enlarge_countries.py
@@ -1,7 +1,21 @@
-import ogr,sys,math, random,os
-import numpy as np
+#Copyright (C) 2016, 2017 Drazen Tutic, Tomislav Jogun, Ana Kuvezdic Divjak
+#This file is part of OSMPoliticalMap software.
+#
+#OSMPoliticalMap is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#OSMPoliticalMap is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with OSMPoliticalMap.  If not, see <http://www.gnu.org/licenses/>.
 
-  
+import ogr,sys,math,random,os
+import numpy as np
 
 def Clean_Enlarge_Polygon(geom):  
     #classify country type and flag the strategy
