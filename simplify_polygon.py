4176eb3fb8c7252424466398487e05b10b8716ca
Dražen Tutić <dtutic@geof.hr>
2017-02-17 11:33:57 +0100

    Update simplify_polygon.py

--- simplify_polygon.py
+++ simplify_polygon.py
@@ -1,6 +1,23 @@
+#Copyright (C) 2016, 2017 Drazen Tutic, Tomislav Jogun, Ana Kuvezdic Divjak
+#This file is part of OSMPoliticalMap software.
+#
+#OSMPoliticalMap is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#OSMPoliticalMap is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with OSMPoliticalMap.  If not, see <http://www.gnu.org/licenses/>.
+
 import ogr,sys,urllib2,csv
 
-# Usage: simplify_polygon.py threshold_distance threshold_area shapefile.shp
+# Usage: simplify_polygon.py threshold_distance min_area shapefile.shp
+# if threshold_distance equals 0 then only removal of features smaller then min_area is performed
 tol = float(sys.argv[1])
 small = float(sys.argv[2])
 
@@ -12,12 +29,11 @@ layer = dataSource.GetLayer()
 
 for feature in layer:
     geom = feature.GetGeometryRef()
-    simpl = geom.SimplifyPreserveTopology(tol)
     try:
-        if simpl.Area() < small:
+        if geom.Area() < small:
             layer.DeleteFeature(feature.GetFID())
         else:
-            feature.SetGeometryDirectly(simpl)
+            feature.SetGeometryDirectly(geom.SimplifyPreserveTopology(tol))
             layer.SetFeature(feature)
     except:
         layer.DeleteFeature(feature.GetFID())     
@@ -25,6 +41,3 @@ for feature in layer:
 
 dataSource.SyncToDisk()
 dataSource.Destroy()
-
-	
-
