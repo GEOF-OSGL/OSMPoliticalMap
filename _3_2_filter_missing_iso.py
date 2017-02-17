0b16f50ac5297deff2a6e0bc622ad83d49e6350d
Dražen Tutić <dtutic@geof.hr>
2017-02-17 11:20:08 +0100

    Update _3_2_filter_missing_iso.py

--- _3_2_filter_missing_iso.py
+++ _3_2_filter_missing_iso.py
@@ -1,3 +1,19 @@
+#Copyright (C) 2016, 2017 Drazen Tutic, Tomislav Jogun, Ana Kuvezdic Divjak
+#This file is part of OSMPoliticalMap software.
+#
+#OSMPoliticalMap is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#OSMPoliticalMap is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with OSMPoliticalMap.  If not, see <http://www.gnu.org/licenses/>.
+
 import ogr,sys,urllib2,csv
 
 # Usage: missiong_iso_polygons.py shapefile.shp
@@ -36,12 +52,12 @@ for c in data:
     if not (c[1] in iso):
         missing.append(c[1])
       
-print "ogr2ogr -f \"ESRI Shapefile\" -where \"('boundary'='administrative' OR 'boundary'='territorial' OR 'boundary'='region') AND (",
+print "ogr2ogr --config OSM_CONFIG_FILE ../osmconf.ini -f \"ESRI Shapefile\" -sql \"SELECT * FROM multipolygons WHERE (boundary='administrative' OR boundary='territorial' OR boundary='region') AND (",
 for c in missing:
   if ((c != missing[-1])):
-    print "'ISO3166-1:alpha2'='"+c+"' OR 'ISO3166-1'='"+c+"' OR ",
+    print "\\\"ISO3166-1_alpha2\\\"='"+c+"' OR \\\"ISO3166-1\\\"='"+c+"' OR ",
   else:
-    print "'ISO3166-1:alpha2'='"+c+"' OR 'ISO3166-1'='"+c+"')\" ",
+    print "\\\"ISO3166-1_alpha2\\\"='"+c+"' OR \\\"ISO3166-1\\\"='"+c+"')\" ",
     print "-overwrite -skipfailures -nlt MULTIPOLYGON -lco ENCODING=UTF-8 03_countries_ex osm_countries.osm"
 
 
