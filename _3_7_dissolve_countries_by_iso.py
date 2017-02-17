a52629a0b9c51eeef22737cf24771c07dc3b7a32
Dražen Tutić <dtutic@geof.hr>
2017-02-17 11:22:12 +0100

    Update _3_7_dissolve_countries_by_iso.py

--- _3_7_dissolve_countries_by_iso.py
+++ _3_7_dissolve_countries_by_iso.py
@@ -1,4 +1,20 @@
-import urllib2, csv, ogr,sys,os
+#Copyright (C) 2016, 2017 Drazen Tutic, Tomislav Jogun, Ana Kuvezdic Divjak
+#This file is part of OSMPoliticalMap software.
+#
+#OSMPoliticalMap is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#OSMPoliticalMap is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with OSMPoliticalMap.  If not, see <http://www.gnu.org/licenses/>.
+
+import urllib2,csv,ogr,sys,os
 
 iso_list = urllib2.urlopen('https://raw.githubusercontent.com/datasets/country-list/master/data.csv')
 reader = csv.reader(iso_list)
@@ -60,6 +76,13 @@ for out_feat in mfeat:
         outLayer.CreateFeature(out_feat)
         out_feat.Destroy()
     i = i+1
+
+#create .prj file
+spatialRef = inLayer.GetSpatialRef()
+spatialRef.MorphToESRI()
+file = open(os.path.splitext(sys.argv[4])[0]+'.prj', 'w')
+file.write(spatialRef.ExportToWkt())
+file.close()
 
 inDataSet.Destroy()
 outDataSet.Destroy()
