3d1c183b8859e2d876b87e3b2145c12a59f4054b
Dražen Tutić <dtutic@geof.hr>
2017-02-17 11:27:05 +0100

    Update _7_Oceans.sh

--- _7_Oceans.sh
+++ _7_Oceans.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-cd ./PK
+#Copyright (C) 2016, 2017 Drazen Tutic, Tomislav Jogun, Ana Kuvezdic Divjak
+#This file is part of OSMPoliticalMap software.
+#
+#OSMPoliticalMap is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#OSMPoliticalMap is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with OSMPoliticalMap.  If not, see <http://www.gnu.org/licenses/>.
 
+cd $OSM_DIR
+echo
 echo "--------------------------------------------------------------------------------------------------------"
 echo "Creating oceans and seas labels ..."
 echo "RESULTING FILE FOR COASTLINES: 07_oceans/oceans_final.shp"
@@ -10,24 +26,21 @@ echo "--------------------------------------------------------------------------
 echo "Filtering: Oceans, Seas, Bays ..."
 osmfilter planet.osm.o5m --keep= --keep-nodes="place=ocean or place=sea or natural=sea or place=bay" -o=osm_oceans.osm
 
-#TODO: check config osmconf.ini to include tags: name,name:en,name:hr
 echo "Converting oceans to shapefile ..."
-ogr2ogr -f "ESRI Shapefile" -where "'name'!=''" -overwrite -skipfailures -nlt POINT -lco ENCODING=UTF-8 07_oceans osm_oceans.osm
+ogr2ogr --config OSM_CONFIG_FILE ../osmconf.ini -f "ESRI Shapefile" -sql "SELECT * FROM points WHERE name != ''" -overwrite -skipfailures -nlt POINT -lco ENCODING=UTF-8 07_oceans osm_oceans.osm
 
-echo "Reprojecting oceans to Winkel Tripel projection ..."
-python ../reproject_to_winkel.py 07_oceans/points.shp 07_oceans/oceans_final.shp
+echo "Reprojecting oceans to map projection ..."
+python ../reproject.py 07_oceans/points.shp 07_oceans/oceans_final.shp
 
 echo "Cleaning unnecessary files: OGR shapefiles, filtered data in osm"
-rm 07_oceans/multilinestrings.* -f
-rm 07_oceans/multipolygons.* -f
-rm 07_oceans/lines.* -f
-rm 07_oceans/other_relations.* -f
 rm osm_oceans.osm -f
 rm 07_oceans/points.* -f
 
 echo "Setting attribute fields ..."
-python ../set_fields.py osm_id,name,name_en,name_hr labels=yes 07_oceans/oceans_final.shp
+python ../set_fields.py osm_id,name,name_$OSM_LANG labels=yes 07_oceans/oceans_final.shp
 
 echo "Oceans and Seas ... Done!"
 echo "------------------------------------------------------------------------------"
+echo
 
+cd ..
